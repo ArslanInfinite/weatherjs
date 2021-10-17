2e4e7c575cff8f8ce0319ae3a3407e00311008ae
Arslan <ali.arslan319@gmail.com>
2021-10-16 23:24:45 -0400

    create getWeather to fetch data from api, convert it to json, and returncurrent weather for location, add function called changeLocation to change location

---
+++ weather.js
@@ -0,0 +1,22 @@
+// getting the api key from the api itself, then setting the attributes that the Weather class will have
+class Weather{
+    constructor(city, state){
+        this.apiKey = '2d6ed8e7c47bf7c66104975f06f96f79'
+        this.city = city 
+        this.state = state 
+    }
+
+    // fetching weather information from API using an async function 
+    async getWeather(){
+        const response = fetch(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}.json`)
+        const responseData = await response.json()
+
+        return responseData.current
+    }
+
+    // changing weather location
+
+    changeLocation(city){
+        this.city = city 
+    }
+}
